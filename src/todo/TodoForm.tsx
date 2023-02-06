487a24c63935c12013f72e1ca2f9fbb5f23e7252
Sunil Joshi <joshisunil2601@gmail.com>
2023-02-06 21:57:47 +0530

    todoForm

--- src/todo/TodoForm.tsx
+++ src/todo/TodoForm.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 import { TiDeleteOutline } from "react-icons/ti";
+import Button from "./Button";
 const TodoForm = () => {
-  return <div></div>;
+  return (
+    <div className="border border-gray-200 rounded-md shadow-md p-4  mt-4">
+      <h3 title="Create a todo"></h3>
+      <div className="mt-4">
+        <input />
+      </div>
+      <div className="space-x-6 mt-4 ">
+        <Button>Save</Button>
+        <Button them="secondary">Cancel</Button>
+      </div>
+    </div>
+  );
 };
 
 export default TodoForm;

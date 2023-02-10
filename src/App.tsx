51bf3f1113b59e5e9dd416ef3678d20914e21a2d
Sunil Joshi <joshisunil2601@gmail.com>
2023-02-10 21:25:35 +0530

    todo

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,37 @@ import NavBar from "./nav/NavBar";
 import FormValidation from "./FormValidation";
 import TodoRow from "./todo/TodoRow";
 import TodoPage from "./todo/TodoPage";
-
+type Aa = {
+  a: string;
+  b: number;
+  c: boolean;
+};
 function App() {
-  const object1 = [
-    {
-      a: "somestring",
+  let object1 = {
+    1: {
+      a: "sumit",
       b: 42,
       c: false,
     },
-  ];
+    2: {
+      a: "somestring",
+      b: 40,
+      c: true,
+    },
+    3: {
+      a: "deepak",
+      b: 42,
+      c: false,
+    },
+  };
+  const value = object1[2];
+  console.log("value", value);
   const obj = Object.keys(object1);
-  console.log(obj);
-  const a = obj.map((s: any) => object1[s]);
-  console.log("keys", a);
-
+  console.log("aaa", obj);
+  const a = obj.map((s) => {
+    return object1[s] as Aa;
+  });
+  console.log("A", a);
   const [count, setCount] = useState(0);
 
   return (
